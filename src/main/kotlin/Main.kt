71dd483ebae5c0b164e24b3cedfe4adf88494ef5
freedasd <iya_str11@mail.ru>
2022-09-17 22:42:50 +0500

    17.09.2022

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+
+    val solution = Solution()
+    val array = intArrayOf(2, 3, 5, 4, 2)
+    val target = 9
+    solution.twoSum(array, target)
+}
---
+++ src/main/kotlin/TwoSum_Easy_17092022.kt
@@ -0,0 +1,22 @@
+class Solution {
+
+    // solved in 2 hours
+
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+
+        var result = intArrayOf()
+        var flag = false
+
+        loop@ for (i in nums) {
+            if (flag) break@loop
+            loops@ for (j in nums.indexOf(i) + 1 until nums.size) {
+                if (i + nums[j] == target) {
+                    flag = true
+                    result = intArrayOf(nums.indexOf(i), j)
+                    break@loops
+                }
+            }
+        }
+        return result
+    }
+}
